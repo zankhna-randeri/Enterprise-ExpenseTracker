e87f20402de6143f4cbd188dc81ed585715bd41f
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-06 16:51:43 -0700

    Singleton class to handle service call

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Utility.kt
@@ -0,0 +1,31 @@
+package com.avengers.enterpriseexpensetracker.util
+
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.avengers.enterpriseexpensetracker.service.LoginService
+
+class Utility {
+
+    companion object {
+        private var instance: Utility? = null
+
+        @Synchronized
+        fun getInstance(): Utility {
+            if (instance == null) {
+                instance = Utility()
+            }
+            return instance!!
+        }
+    }
+
+
+    fun showMsg(context: Context?, msg: String?) {
+        Toast.makeText(context, msg,
+                Toast.LENGTH_SHORT).show()
+    }
+
+    fun startExpenseTrackerService(context: Context?, intent: Intent?) {
+       LoginService.enqueueWork(context, intent)
+    }
+}

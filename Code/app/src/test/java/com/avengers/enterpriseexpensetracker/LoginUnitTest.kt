415dad84c148ac3a2c5bb65c8a34e5a8a97b34a0
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-06 18:52:46 -0700

    Added tests for email validation

---
+++ Code/app/src/test/java/com/avengers/enterpriseexpensetracker/LoginUnitTest.kt
@@ -0,0 +1,51 @@
+package com.avengers.enterpriseexpensetracker
+
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+/**
+ * Login screen unit tests.
+ */
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.P], manifest = Config.NONE)
+class LoginUnitTest {
+
+    private val loginActivity = LoginActivity()
+
+    @Test
+    fun emptyEmailWithWhiteSpaceTest() {
+        val email = "   "
+        assert(!loginActivity.isValidEmail(email))
+    }
+
+    @Test
+    fun emptyEmailTest() {
+        val email = ""
+        assert(!loginActivity.isValidEmail(email))
+    }
+
+    @Test
+    fun invalidEmailTest1() {
+        val email = "abc@abc.com@"
+        assert(!loginActivity.isValidEmail(email))
+    }
+
+    @Test
+    fun invalidEmailTest2() {
+        assert(!loginActivity.isValidEmail("abcabc.abc.com"))
+    }
+
+    @Test
+    fun invalidEmailTest3() {
+        assert(!loginActivity.isValidEmail("abc@@abc.abc.com"))
+    }
+
+    @Test
+    fun validEmailTest() {
+        val email = "abc@abc.com"
+        assert(loginActivity.isValidEmail(email))
+    }
+}

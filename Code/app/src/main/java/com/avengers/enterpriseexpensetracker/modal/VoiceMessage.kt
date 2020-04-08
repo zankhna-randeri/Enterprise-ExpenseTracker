a738e05a64f5ba2318785650400e13ef6cc78a72
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-08 16:34:08 -0700

    Class to handle voice bot interaction

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/modal/VoiceMessage.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/modal/VoiceMessage.kt
@@ -1,4 +1,4 @@
 package com.avengers.enterpriseexpensetracker.modal
 
-data class VoiceMessage(private var text: String? = "",
-                        private var isResponse: Boolean = false)
+data class VoiceMessage(val text: String,
+                        val isResponse: Boolean = false)

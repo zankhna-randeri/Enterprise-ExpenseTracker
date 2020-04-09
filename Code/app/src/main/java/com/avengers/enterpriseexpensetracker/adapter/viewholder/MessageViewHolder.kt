fd86dcd18256f83560e03cfc5d6199469e6f6ac4
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-09 13:39:27 -0700

    Viewholder to bind message list with UI

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/viewholder/MessageViewHolder.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/viewholder/MessageViewHolder.kt
@@ -5,8 +5,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.avengers.enterpriseexpensetracker.R
 
-class ReqMsgViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-    private var reqMsg: TextView = itemView.findViewById(R.id.txtMessage)
+    private var msgView: TextView = itemView.findViewById(R.id.txtMessage)
 
+    fun getMessageView(): TextView {
+        return msgView
+    }
 }

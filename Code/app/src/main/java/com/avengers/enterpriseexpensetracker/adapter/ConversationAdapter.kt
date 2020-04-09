02b8e15979ab2d19924eb3506a46205d25153e7d
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-09 13:39:01 -0700

    Adapter to hold voice chat conversation

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/ConversationAdapter.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/ConversationAdapter.kt
@@ -1,4 +1,46 @@
 package com.avengers.enterpriseexpensetracker.adapter
 
-class ConversationAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.adapter.viewholder.MessageViewHolder
+import com.avengers.enterpriseexpensetracker.modal.VoiceMessage
+
+class ConversationAdapter(private var messages: MutableList<VoiceMessage>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val REQUEST = 0
+    private val RESPONSE = 1
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        lateinit var viewHolder: RecyclerView.ViewHolder
+        val inflater = LayoutInflater.from(parent.context)
+        when (viewType) {
+            REQUEST -> {
+                val reqViewHolder = inflater.inflate(R.layout.item_voice_request, parent, false)
+                viewHolder = MessageViewHolder(reqViewHolder)
+            }
+            RESPONSE -> {
+                val resViewHolder = inflater.inflate(R.layout.item_voice_response, parent, false)
+                viewHolder = MessageViewHolder(resViewHolder)
+            }
+        }
+
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return messages.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as MessageViewHolder).getMessageView().text = messages[position].text
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (messages[position].isResponse) {
+            RESPONSE
+        } else
+            REQUEST
+    }
 }

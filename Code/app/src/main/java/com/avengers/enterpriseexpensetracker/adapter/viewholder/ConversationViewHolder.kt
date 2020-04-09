79b33971d96390a5bf43d874a2379adc6e1bbd42
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-09 13:29:49 -0700

    Dependency for viewmodel and livedata

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/viewholder/ConversationViewHolder.kt
+++
@@ -1,2 +0,0 @@
-package com.avengers.enterpriseexpensetracker.adapter.viewholder
-
---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/viewholder/MessageViewHolder.kt
@@ -0,0 +1,12 @@
+package com.avengers.enterpriseexpensetracker.adapter.viewholder
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.avengers.enterpriseexpensetracker.R
+
+class ReqMsgViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    private var reqMsg: TextView = itemView.findViewById(R.id.txtMessage)
+
+}

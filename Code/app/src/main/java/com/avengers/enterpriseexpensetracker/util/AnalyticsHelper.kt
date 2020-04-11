4fa2754f1b8f9c7c8a2011c258d2ca6b800d5008
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-11 01:40:15 -0700

    Helper class to log firebase events

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/AnalyticsHelper.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/AnalyticsHelper.kt
@@ -1,4 +1,36 @@
 package com.avengers.enterpriseexpensetracker.util
 
+import android.content.Context
+import android.os.Bundle
+import com.avengers.enterpriseexpensetracker.modal.tracking.TrackLoginData
+import com.avengers.enterpriseexpensetracker.modal.tracking.TrackScreenData
+import com.google.firebase.analytics.FirebaseAnalytics
+
 class AnalyticsHelper {
+    companion object {
+        private var instance: AnalyticsHelper? = null
+        private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+        @Synchronized
+        fun getInstance(): AnalyticsHelper {
+            if (instance == null) {
+                instance = AnalyticsHelper()
+            }
+            return instance!!
+        }
+    }
+
+    fun trackViewScreenEvent(context: Context, trackingData: TrackScreenData) {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(context)
+        val bundle = Bundle()
+        bundle.putString("screenName", trackingData.getScreenName())
+        firebaseAnalytics.logEvent("view_screen", bundle)
+    }
+
+    fun trackLogin(context: Context, trackingData: TrackLoginData) {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(context)
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, Bundle().apply {
+            putString(FirebaseAnalytics.Param.METHOD, trackingData.getMethod())
+        })
+    }
 }

1c453c25c154fbe2b3087b7d059a6d41c1a1c1e1
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-12 03:26:15 -0700

    Currency formatter for pie chart data

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/CurrencyFormatter.kt
@@ -0,0 +1,29 @@
+package com.avengers.enterpriseexpensetracker.util
+
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import java.text.NumberFormat
+import java.util.*
+
+class CurrencyFormatter : ValueFormatter {
+    private var format: NumberFormat? = null
+    private var pieChart: PieChart? = null
+
+    constructor() : super() {
+        format = NumberFormat.getCurrencyInstance(Locale.getDefault())
+        format?.minimumFractionDigits = 2
+    }
+
+    constructor(pieChart: PieChart) : this() {
+        this.pieChart = pieChart
+    }
+
+    override fun getFormattedValue(value: Float): String {
+        return format?.format(value).toString()
+    }
+
+    override fun getPieLabel(value: Float, pieEntry: PieEntry?): String {
+        return format?.format(value).toString()
+    }
+}

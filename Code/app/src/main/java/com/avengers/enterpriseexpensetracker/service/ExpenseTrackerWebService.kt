c8440d0b143340a02242ef17139b7a6ec8c103a5
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-14 14:43:28 -0700

    Added api call for upload receipt

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/service/ExpenseTrackerWebService.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/service/ExpenseTrackerWebService.kt
@@ -2,16 +2,20 @@ package com.avengers.enterpriseexpensetracker.service
 
 import com.avengers.enterpriseexpensetracker.modal.LoginUser
 import com.avengers.enterpriseexpensetracker.modal.response.LoginResponse
+import com.avengers.enterpriseexpensetracker.modal.response.ReceiptScanResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ExpenseTrackerWebService {
     companion object {
         private const val BASE_URL = "https://sumanthravipati-sjsu.online"
-
         val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -20,4 +24,10 @@ interface ExpenseTrackerWebService {
 
     @POST("/userLogin")
     fun loginUser(@Body userLoginRequest: LoginUser): Call<LoginResponse>
+
+    @Multipart
+    @POST("uploadReceiptToS3")
+    fun uploadReceipt(@Part file: MultipartBody.Part,
+                      @Part("emailId") emailId: RequestBody,
+                      @Part("expenseCategory") category: RequestBody): Call<ReceiptScanResponse>
 }

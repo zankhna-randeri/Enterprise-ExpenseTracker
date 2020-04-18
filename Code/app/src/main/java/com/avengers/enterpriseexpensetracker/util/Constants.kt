ec27b234c51be0fc2c6f0175948119f42d001042
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-18 01:31:48 -0700

    Added constants related to Submit expense api
    - Extra
    - Action
    - Response broadcast

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Constants.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Constants.kt
@@ -20,11 +20,14 @@ class Constants {
         // Actions
         const val ACTION_LOGIN = "com.avengers.enterpriseexpensetracker.action.LOGIN"
         const val ACTION_UPLOAD = "com.avengers.enterpriseexpensetracker.action.UPLOAD"
+        const val ACTION_SUBMIT_EXPENSE_REPORT =
+            "com.avengers.enterpriseexpensetracker.action.SUBMIT_EXPENSE_REPORT"
 
         // Extras
         const val EXTRA_LOGIN_USER = "login_user"
         const val EXTRA_UPLOAD_RECEIPT_PATH = "receipt_path"
         const val EXTRA_UPLOAD_EXPENSE_TYPE = "expense_type"
+        const val EXTRA_SUBMIT_EXPENSE_REPORT = "expense_report"
         const val EXTRA_API_RESPONSE = "api_response"
 
         // Broadcast
@@ -32,5 +35,7 @@ class Constants {
             "com.avengers.enterpriseexpensetracker.service.action.LOGIN_RESPONSE"
         const val BROADCAST_RECEIPT_SCAN_RESPONSE =
             "com.avengers.enterpriseexpensetracker.service.action.RECEIPT_SCAN_RESPONSE"
+        const val BROADCAST_SUBMIT_EXPENSE_REPORT_RESPONSE =
+            "com.avengers.enterpriseexpensetracker.service.action.SUBMIT_EXPENSE_REPORT_RESPONSE"
     }
 }

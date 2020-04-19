c1fc8f329ce167955780575d738b5a00371f6a7e
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-18 20:25:46 -0700

    Changed toast duration to long

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Utility.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Utility.kt
@@ -22,7 +22,7 @@ class Utility {
 
     fun showMsg(context: Context, msg: String?) {
         Toast.makeText(context, msg,
-                Toast.LENGTH_SHORT).show()
+                Toast.LENGTH_LONG).show()
     }
 
     fun startExpenseTrackerService(context: Context?, intent: Intent?) {

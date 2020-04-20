3277eaacd149f97bfa1d23597e3ba2d5155e6d56
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-19 20:42:38 -0700

    Added action for category wise total api call

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Constants.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Constants.kt
@@ -25,9 +25,11 @@ class Constants {
 
         // Actions
         const val ACTION_LOGIN = "com.avengers.enterpriseexpensetracker.action.LOGIN"
-        const val ACTION_UPLOAD = "com.avengers.enterpriseexpensetracker.action.UPLOAD"
+        const val ACTION_RECEIPT_SCAN = "com.avengers.enterpriseexpensetracker.action.UPLOAD"
         const val ACTION_SUBMIT_EXPENSE_REPORT =
             "com.avengers.enterpriseexpensetracker.action.SUBMIT_EXPENSE_REPORT"
+        const val ACTION_CATEGORY_TOTAL_EXPENSE =
+            "com.avengers.enterpriseexpensetracker.action.CATEGORY_TOTAL_EXPENSE"
 
         // Extras
         const val EXTRA_LOGIN_USER = "login_user"
@@ -43,5 +45,7 @@ class Constants {
             "com.avengers.enterpriseexpensetracker.service.action.RECEIPT_SCAN_RESPONSE"
         const val BROADCAST_SUBMIT_EXPENSE_REPORT_RESPONSE =
             "com.avengers.enterpriseexpensetracker.service.action.SUBMIT_EXPENSE_REPORT_RESPONSE"
+        const val BROADCAST_CATEGORY_TOTAL_EXPENSE_RESPONSE =
+            "com.avengers.enterpriseexpensetracker.service.action.CATEGORY_TOTAL_EXPENSE_RESPONSE"
     }
 }

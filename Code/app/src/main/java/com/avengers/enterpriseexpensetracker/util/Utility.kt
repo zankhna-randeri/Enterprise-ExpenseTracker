d2e1f8a88501aeeadf5cf52e5f3718a783bcac21
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-19 17:51:11 -0700

    Code to hide keyboard

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Utility.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/util/Utility.kt
@@ -1,7 +1,10 @@
 package com.avengers.enterpriseexpensetracker.util
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.avengers.enterpriseexpensetracker.service.EETrackerJobService
 
@@ -19,13 +22,23 @@ class Utility {
         }
     }
 
-
     fun showMsg(context: Context, msg: String?) {
         Toast.makeText(context, msg,
                 Toast.LENGTH_LONG).show()
     }
 
     fun startExpenseTrackerService(context: Context?, intent: Intent?) {
-       EETrackerJobService.enqueueWork(context, intent)
+        EETrackerJobService.enqueueWork(context, intent)
+    }
+
+    fun hideKeyboard(activity: Activity) {
+        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }

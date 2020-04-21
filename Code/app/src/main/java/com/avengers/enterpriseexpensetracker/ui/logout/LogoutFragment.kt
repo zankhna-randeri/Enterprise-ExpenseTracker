a292ee63e471dc439f4e005d9191f3361a6cb4ce
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-21 16:11:02 -0700

    Added logout functionality

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/logout/LogoutFragment.kt
@@ -0,0 +1,40 @@
+package com.avengers.enterpriseexpensetracker.ui.logout
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.avengers.enterpriseexpensetracker.LoginActivity
+import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.util.EETrackerPreferenceManager
+import com.avengers.enterpriseexpensetracker.util.Utility
+
+class LogoutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_logout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logout()
+    }
+
+    private fun logout() {
+        activity?.applicationContext?.let { context ->
+            EETrackerPreferenceManager.logout(context)
+            Utility.getInstance().showMsg(context, getString(R.string.txt_logout_done))
+        }
+
+        val intent = Intent(activity, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        activity?.finish()
+    }
+}

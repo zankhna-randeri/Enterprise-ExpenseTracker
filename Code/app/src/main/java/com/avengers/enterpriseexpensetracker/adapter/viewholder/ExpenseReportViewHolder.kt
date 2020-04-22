c00ad435e11319f17822a4b52ff55e7c11b86214
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-21 21:03:28 -0700

    Added button click listener in viewholder

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/viewholder/ExpenseReportViewHolder.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/viewholder/ExpenseReportViewHolder.kt
@@ -1,16 +1,26 @@
 package com.avengers.enterpriseexpensetracker.adapter.viewholder
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.adapter.ItemClickListener
+import java.lang.ref.WeakReference
 
-class ExpenseReportViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ExpenseReportViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
     private var txtDate = itemView.findViewById<TextView>(R.id.txt_date)
     private var txtName = itemView.findViewById<TextView>(R.id.txt_name)
     private var txtAmount = itemView.findViewById<TextView>(R.id.txt_amount)
     private var btnDelete = itemView.findViewById<ImageButton>(R.id.btnDelete)
+
+    private var clickListener: WeakReference<ItemClickListener>? = null
+
+    constructor(itemView: View, clickListener: ItemClickListener) : this(itemView) {
+        this.clickListener = WeakReference(clickListener)
+    }
 
     fun getDateView(): TextView {
         return txtDate
@@ -26,5 +36,10 @@ class ExpenseReportViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
 
     fun getDeleteView(): ImageButton {
         return btnDelete
+    }
+
+    override fun onClick(v: View?) {
+        Log.e("EETracker *******", "Adapter position on click: $adapterPosition")
+        clickListener?.get()?.onPositionClickListener(adapterPosition)
     }
 }

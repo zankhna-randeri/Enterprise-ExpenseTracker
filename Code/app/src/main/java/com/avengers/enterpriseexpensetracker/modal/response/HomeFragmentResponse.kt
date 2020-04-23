e1ab61a89a43bdbb9f7572c669f39c8b39ac810f
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-22 23:39:54 -0700

    Added secondary constructor

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/modal/response/HomeFragmentResponse.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/modal/response/HomeFragmentResponse.kt
@@ -3,6 +3,9 @@ package com.avengers.enterpriseexpensetracker.modal.response
 import com.avengers.enterpriseexpensetracker.modal.ExpenseReport
 
 data class HomeFragmentResponse(var categoryWiseExpense: CategoryWiseTotalResponse?,
-                                var expenseReports: List<ExpenseReport>?) : ApiResponse()
+                                var expenseReports: MutableList<ExpenseReport>?) : ApiResponse() {
+
+    constructor() : this(null, null)
+}
 
 data class GetAllReportsResponse(var reports: List<ExpenseReport>?) : ApiResponse()

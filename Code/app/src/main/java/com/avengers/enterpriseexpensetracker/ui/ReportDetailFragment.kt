655bd01742bf5d4ee372c56be62f446c4b52c806
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-23 16:46:47 -0700

    Report Detail fragment
    - Code to handle navigation action args from source

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/ReportDetailFragment.kt
@@ -0,0 +1,26 @@
+package com.avengers.enterpriseexpensetracker.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.modal.ExpenseReport
+
+class ReportDetailFragment : Fragment() {
+    private lateinit var report: ExpenseReport
+    private val args: ReportDetailFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        report = args.report
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_report_detail, container, false)
+    }
+}

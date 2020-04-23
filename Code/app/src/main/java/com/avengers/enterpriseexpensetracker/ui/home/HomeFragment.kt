d120cdec03fc846ad5b99288e0d8b050835bea32
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-22 23:43:40 -0700

    Moved fetch home functionality to viewmodel
    - Added observer on getallreports and piechart data
    - Removed broadcast receiver

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/home/HomeFragment.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/home/HomeFragment.kt
@@ -1,18 +1,15 @@
 package com.avengers.enterpriseexpensetracker.ui.home
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -20,30 +17,24 @@ import com.avengers.enterpriseexpensetracker.R
 import com.avengers.enterpriseexpensetracker.adapter.HomeViewExpenseAdapter
 import com.avengers.enterpriseexpensetracker.adapter.ItemButtonClickListener
 import com.avengers.enterpriseexpensetracker.modal.ExpenseReport
-import com.avengers.enterpriseexpensetracker.modal.response.ApiResponse
 import com.avengers.enterpriseexpensetracker.modal.response.CategoryWiseTotalResponse
-import com.avengers.enterpriseexpensetracker.modal.response.HomeFragmentResponse
 import com.avengers.enterpriseexpensetracker.modal.tracking.TrackScreenData
-import com.avengers.enterpriseexpensetracker.receiver.ApiResponseReceiver
-import com.avengers.enterpriseexpensetracker.service.EETrackerJobService
 import com.avengers.enterpriseexpensetracker.util.AnalyticsHelper
-import com.avengers.enterpriseexpensetracker.util.Constants
+import com.avengers.enterpriseexpensetracker.util.EETrackerPreferenceManager
 import com.avengers.enterpriseexpensetracker.util.NetworkHelper
-import com.avengers.enterpriseexpensetracker.util.Utility
 
 class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     private lateinit var homeViewModel: HomeViewModel
     private var pendingExpenseView: RecyclerView? = null
     private var swipeRefreshLayout: SwipeRefreshLayout? = null
-    private var homeScreenResponse: HomeFragmentResponse? = null
-    private var homeScreenResponseReceiver: BroadcastReceiver? = null
-    private var pendingExpenses: MutableList<ExpenseReport>? = null
+
     private var adapter: HomeViewExpenseAdapter? = null
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
         return inflater.inflate(R.layout.fragment_reports, container, false)
     }
 
@@ -54,27 +45,22 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         }
 
         initView(view)
-        intiBroadcastReceiver()
         initObserver()
+
+
+        fetchHomeScreenData()
     }
 
     override fun onResume() {
         super.onResume()
-        homeScreenResponseReceiver?.let { receiver ->
-            val intentFilter = IntentFilter(Constants.BROADCAST_HOME_DATA_RESPONSE)
-            activity?.applicationContext?.let { context ->
-                LocalBroadcastManager.getInstance(context).registerReceiver(receiver, intentFilter)
-            }
-        }
+        Log.d("EETracker *******", "Invoked onResume()")
+        initObserver()
+        homeViewModel.addMergeDataSources()
     }
 
     override fun onPause() {
         super.onPause()
-        homeScreenResponseReceiver?.let { receiver ->
-            activity?.applicationContext?.let { context ->
-                LocalBroadcastManager.getInstance(context).unregisterReceiver(receiver)
-            }
-        }
+        Log.d("EETracker *******", "Invoked onPause()")
     }
 
     override fun onRefresh() {
@@ -89,79 +75,22 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         swipeRefreshLayout?.setColorSchemeResources(R.color.colorPrimary,
                 R.color.color_chart_1,
                 R.color.color_chart_3)
-
-        fetchHomeScreenData()
-    }
-
-    private fun intiBroadcastReceiver() {
-        homeScreenResponseReceiver = object : ApiResponseReceiver() {
-            override fun onSuccess(context: Context?, response: ApiResponse) {
-                homeScreenResponse = response as HomeFragmentResponse
-                val categoryWiseTotal = homeScreenResponse?.categoryWiseExpense
-                val allExpenseReports = homeScreenResponse?.expenseReports
-                pendingExpenses = fetchPendingExpenses(allExpenseReports)
-
-                // only check approved expenses. If there is no approved expenses,
-                // means `categoryWiseTotal` must be 0.0 for all
-                if (pendingExpenses.isNullOrEmpty()) {
-                    // TODO: Show empty view
-                    // showEmptyView()
-                } else {
-                    bindExpenseView(categoryWiseTotal!!, pendingExpenses!!)
-                }
-            }
-
-            override fun onFailure(context: Context?, message: String?) {
-                context?.let { Utility.getInstance().showMsg(it, message) }
-            }
-
-            override fun onReceive(context: Context?, intent: Intent?) {
-                swipeRefreshLayout?.isRefreshing = false
-
-                val homeFragmentResponse =
-                    intent?.getParcelableExtra<HomeFragmentResponse>(Constants.EXTRA_API_RESPONSE)
-
-                var categoryStatus = false
-                homeFragmentResponse?.let { response ->
-                    Log.d("EETracker *******", "response $response")
-                    response.expenseReports?.let { allReports ->
-                        categoryStatus = response.categoryWiseExpense?.getApiResponseStatus() ?: false
-                        if (categoryStatus && !allReports.isNullOrEmpty()) {
-                            onSuccess(context, response)
-                        } else {
-                            onFailure(context, context?.getString(R.string.txt_api_failed))
-                        }
-                    } ?: run {
-                        // TODO: Show empty view
-                        // showEmptyView()
-                    }
-                }
-            }
-        }
     }
 
     private fun initObserver() {
-        homeViewModel.getPendingExpense()
+        homeViewModel.getHomeScreenData().observe(viewLifecycleOwner, Observer { homeMergedData ->
+            Log.d("EETracker *******", "Invoked observer on getHomeScreenData()")
+            if (homeMergedData.categoryWiseExpense != null && !homeMergedData.expenseReports.isNullOrEmpty()) {
+                swipeRefreshLayout?.isRefreshing = false
+                bindExpenseView(homeMergedData.categoryWiseExpense!!, homeMergedData.expenseReports!!)
+            }
+        })
+        homeViewModel.getPendingExpenses()
                 ?.observe(viewLifecycleOwner,
                         Observer {
-                            pendingExpenses?.clear()
-                            if (!it.isNullOrEmpty()) {
-                                pendingExpenses?.addAll(it)
-                            }
+                            Log.d("EETracker *******", "Invoked observer on getPendingExpenses()")
                             adapter?.notifyDataSetChanged()
                         })
-    }
-
-    private fun fetchPendingExpenses(allExpenseReports: List<ExpenseReport>?): MutableList<ExpenseReport> {
-        val pendingExpenses = ArrayList<ExpenseReport>()
-        val iterator = allExpenseReports?.iterator()
-        iterator?.forEach { expenseReport ->
-            if (expenseReport.getReportStatus().equals(Constants.Companion.Status.Pending.name, true)) {
-                pendingExpenses.add(expenseReport)
-            }
-        }
-
-        return pendingExpenses
     }
 
     private fun bindExpenseView(categoryWiseTotal: CategoryWiseTotalResponse,
@@ -172,16 +101,8 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
                         categoryWiseTotal,
                         pendingExpenses,
                         object : ItemButtonClickListener {
-                            override fun onPositionClickListener(position: Int) {
-                                activity?.applicationContext?.let { context ->
-                                    if (NetworkHelper.hasNetworkAccess(context)) {
-                                        val pos = position - 1
-                                        pendingExpenses[pos].getReportId()?.let { id ->
-                                            homeViewModel.deletePendingReport(id, pos)
-                                        }
-                                    }
-                                }
-
+                            override fun onDeleteClickListener(position: Int) {
+                                confirmDelete(position)
                             }
                         })
             }
@@ -191,9 +112,8 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     private fun fetchHomeScreenData() {
         swipeRefreshLayout?.isRefreshing = true
 
-        val intent = Intent(activity?.applicationContext, EETrackerJobService::class.java).apply {
-            action = Constants.ACTION_FETCH_HOME_DATA
+        EETrackerPreferenceManager.getUserEmail(activity?.applicationContext)?.let {
+            homeViewModel.fetchHomeScreenData(it)
         }
-        Utility.getInstance().startExpenseTrackerService(context, intent)
     }
 }

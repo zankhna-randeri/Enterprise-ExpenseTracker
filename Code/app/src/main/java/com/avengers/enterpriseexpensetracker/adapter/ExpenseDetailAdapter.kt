380c370d14208881f1fe4271415ebccebdd77f5d
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-24 00:14:44 -0700

    Code to create spannable string for expense detail

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/ExpenseDetailAdapter.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/ExpenseDetailAdapter.kt
@@ -1,8 +1,14 @@
 package com.avengers.enterpriseexpensetracker.adapter
 
 import android.content.Context
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.avengers.enterpriseexpensetracker.R
 import com.avengers.enterpriseexpensetracker.adapter.viewholder.ExpenseDetailViewHolder
@@ -29,7 +35,7 @@ class ExpenseDetailAdapter(private var context: Context,
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is ExpenseDetailViewHolder) {
             holder.getDateView().text = expenses[position].getDate()
-            holder.getNameView().text = "name"
+            holder.getNameView().text = generateSpannableString(expenses[position])
             holder.getAmountView().text = context.resources.getString(R.string.txt_currency_dollar_amount,
                     expenses[position].getAmount())
             holder.getCategoryView().setImageResource(getCategoryResource(expenses[position]))
@@ -53,5 +59,45 @@ class ExpenseDetailAdapter(private var context: Context,
         }
 
         return R.mipmap.ic_launcher_round
+    }
+
+    private fun generateSpannableString(expense: Expense): CharSequence? {
+
+        var category = getCategoryText(expense) + "\n"
+        var business = if (!expense.getBusinessName().isNullOrEmpty()) {
+            expense.getBusinessName()
+        } else {
+            ""
+        }
+        var source = category + business
+        val spannableString = SpannableString(source)
+        val typeface = ResourcesCompat.getFont(context, R.font.app_font)
+        spannableString.setSpan(typeface?.style?.let { StyleSpan(typeface.style) },
+                0,
+                source.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        // make category bold
+        spannableString.setSpan(StyleSpan(Typeface.BOLD),
+                0,
+                category.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        //make category font bigger
+        spannableString.setSpan(RelativeSizeSpan(2f), 0, category.length, 0)
+        return spannableString
+    }
+
+    private fun getCategoryText(expense: Expense): String {
+        var category = ""
+        when (expense.getCategory()) {
+            Constants.Companion.ExpenseType.Food.name -> {
+                expense.getSubCategory()?.let {
+                    category = expense.getSubCategory().toString()
+                }
+                category += ", " + Constants.Companion.ExpenseType.Food.name
+            }
+        }
+
+        return category
     }
 }

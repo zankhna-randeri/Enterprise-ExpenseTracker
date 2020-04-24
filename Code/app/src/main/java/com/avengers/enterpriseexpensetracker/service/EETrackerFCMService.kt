2d0ead27bc8feca5991b55d0ebc9837460b5c154
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-24 01:19:32 -0700

    Removed unused override function

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/service/EETrackerFCMService.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/service/EETrackerFCMService.kt
@@ -9,29 +9,27 @@ import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import com.avengers.enterpriseexpensetracker.NotificationActivity
 import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.ui.activity.NotificationActivity
+import com.avengers.enterpriseexpensetracker.util.Constants
+import com.avengers.enterpriseexpensetracker.util.Utility
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class EETrackerFCMService : FirebaseMessagingService() {
 
-    override fun onNewToken(token: String) {
-        Log.d("EETracker ***", "Refreshed token: $token")
-    }
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d("EETracker ***", "From: ${remoteMessage.from}")
+        Log.d("EETracker *******", "From: ${remoteMessage.from}")
 
         // Check if message contains a data payload.
         remoteMessage.data.isNotEmpty().let {
-            Log.d("EETracker ***", "Message data payload: " + remoteMessage.data)
+            Log.d("EETracker *******", "Message data payload: " + remoteMessage.data)
 
         }
 
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
-            Log.d("EETracker ***", "Message Notification Body: ${it.body}")
+            Log.d("EETracker *******", "Message Notification Body: ${it.body}")
             it.body?.let { message -> sendNotification(message) }
         }
 

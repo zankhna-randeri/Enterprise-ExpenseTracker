8c9a29657233dd216566e1737ee83aac89c0a52a
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-24 22:48:52 -0700

    Basic init view for change password

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/ChangePasswordFragment.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/ChangePasswordFragment.kt
@@ -5,13 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.avengers.enterpriseexpensetracker.R
+import com.google.android.material.textfield.TextInputLayout
 
-class ChangePasswordFragment : Fragment() {
+class ChangePasswordFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var oldPassword: TextInputLayout
+    private lateinit var newPassword: TextInputLayout
+    private lateinit var confirmNewPassword: TextInputLayout
+    private lateinit var btnSubmit: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_change_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView(view)
+    }
+
+    private fun initView(view: View) {
+        oldPassword = view.findViewById(R.id.txt_input_old_password)
+        newPassword = view.findViewById(R.id.txt_input_new_password)
+        confirmNewPassword = view.findViewById(R.id.txt_input_confirm_new_password)
+        btnSubmit = view.findViewById(R.id.btn_change_pwd_submit)
+        btnSubmit.setOnClickListener(this)
     }
 
 }

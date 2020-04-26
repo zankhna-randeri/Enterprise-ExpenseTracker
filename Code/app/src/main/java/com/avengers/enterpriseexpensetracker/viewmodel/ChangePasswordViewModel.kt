d25c3cbe38459787c1a97777062604d933f72f95
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-25 17:40:35 -0700

    Added retrofit api call for change password
    - Create object of changepassword request
    - Make webservice call

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/ChangePasswordViewModel.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/ChangePasswordViewModel.kt
@@ -1,6 +1,29 @@
 package com.avengers.enterpriseexpensetracker.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.avengers.enterpriseexpensetracker.modal.request.ChangePasswordRequest
+import com.avengers.enterpriseexpensetracker.modal.response.ApiResponse
+import com.avengers.enterpriseexpensetracker.service.EETrackerWebService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ChangePasswordViewModel : ViewModel() {
+
+    private val webservice = EETrackerWebService.retrofit.create(EETrackerWebService::class.java)
+
+    fun changePassword(emailId: String, oldPassword: String, newPassword: String) {
+        val changePasswordReq = ChangePasswordRequest(emailId, oldPassword, newPassword)
+        val call = webservice.changePassword(changePasswordReq)
+        call.enqueue(object : Callback<ApiResponse> {
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
 }

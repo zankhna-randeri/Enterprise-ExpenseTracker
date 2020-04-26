995c57a2f0a653447d7112542afeef699b6d1b3b
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-25 17:48:24 -0700

    Handle success/fail api call and save in livedata

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/ChangePasswordViewModel.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/ChangePasswordViewModel.kt
@@ -1,5 +1,6 @@
 package com.avengers.enterpriseexpensetracker.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.avengers.enterpriseexpensetracker.modal.request.ChangePasswordRequest
 import com.avengers.enterpriseexpensetracker.modal.response.ApiResponse
@@ -11,19 +12,25 @@ import retrofit2.Response
 class ChangePasswordViewModel : ViewModel() {
 
     private val webservice = EETrackerWebService.retrofit.create(EETrackerWebService::class.java)
+    private var apiCallStatus = MutableLiveData<Boolean>()
 
     fun changePassword(emailId: String, oldPassword: String, newPassword: String) {
         val changePasswordReq = ChangePasswordRequest(emailId, oldPassword, newPassword)
         val call = webservice.changePassword(changePasswordReq)
         call.enqueue(object : Callback<ApiResponse> {
             override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                apiCallStatus.postValue(false)
             }
 
             override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
-                TODO("Not yet implemented")
+                response.body()?.let {
+                    apiCallStatus.postValue(it.getStatus())
+                }
             }
         })
+    }
 
+    fun getApiCallStatus(): MutableLiveData<Boolean> {
+        return apiCallStatus
     }
 }

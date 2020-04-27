d210cb51a0e92e050aabe5c909303d06f5a1be86
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-27 15:52:13 -0700

    Notification fragment screen

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/NotificationFragment.kt
@@ -0,0 +1,27 @@
+package com.avengers.enterpriseexpensetracker.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.avengers.enterpriseexpensetracker.R
+
+class NotificationFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        menu.clear()
+    }
+}

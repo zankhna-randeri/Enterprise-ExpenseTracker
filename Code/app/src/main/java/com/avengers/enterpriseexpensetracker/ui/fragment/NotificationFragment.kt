1fd285b10907ef163f9a3f9e6d7b2ae89a7bd502
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-27 16:11:08 -0700

    Viewmodel class for notification

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/NotificationFragment.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/NotificationFragment.kt
@@ -6,9 +6,13 @@ import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.viewmodel.NotificationViewModel
 
 class NotificationFragment : Fragment() {
+
+    private var viewModel: NotificationViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,7 @@ class NotificationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+        viewModel = ViewModelProvider(this).get(NotificationViewModel::class.java)
         return inflater.inflate(R.layout.fragment_notification, container, false)
     }
 
---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/NotificationViewModel.kt
@@ -0,0 +1,8 @@
+package com.avengers.enterpriseexpensetracker.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class NotificationViewModel: ViewModel() {
+
+
+}

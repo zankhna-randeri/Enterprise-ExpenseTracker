a537965f9b4ecf62dbcd82715b8e41d5c4498c20
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-28 00:35:01 -0700

    Delete notification api call in viewmodel

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/NotificationViewModel.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/viewmodel/NotificationViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.avengers.enterpriseexpensetracker.modal.Notification
+import com.avengers.enterpriseexpensetracker.modal.response.ApiResponse
 import com.avengers.enterpriseexpensetracker.service.EETrackerWebService
 import com.avengers.enterpriseexpensetracker.util.Constants
 import retrofit2.Call
@@ -14,6 +15,7 @@ class NotificationViewModel : ViewModel() {
 
     private val webservice = EETrackerWebService.retrofit.create(EETrackerWebService::class.java)
     private var apiResponse = MutableLiveData<MutableList<Notification>>()
+    private var deleteApiResponse = MutableLiveData<ApiResponse>()
 
     fun getAllNotifications(emailId: String) {
         val call = webservice.getNotifications(emailId)
@@ -36,5 +38,25 @@ class NotificationViewModel : ViewModel() {
 
     fun getApiCallStatus(): MutableLiveData<MutableList<Notification>> {
         return apiResponse
+    }
+
+    fun deleteNotification(id: Int) {
+        val call = webservice.deleteNotification(id)
+        call.enqueue(object : Callback<ApiResponse> {
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                deleteApiResponse.postValue(ApiResponse(false))
+            }
+
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                response.body()?.let {
+                    Log.d(Constants.TAG, "API Response deleteNotification: ${response.body().toString()}")
+                    deleteApiResponse.postValue(it)
+                }
+            }
+        })
+    }
+
+    fun getDeleteResponse(): MutableLiveData<ApiResponse> {
+        return deleteApiResponse
     }
 }

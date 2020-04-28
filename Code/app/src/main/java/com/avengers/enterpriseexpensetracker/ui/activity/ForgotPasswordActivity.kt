6704ec8036070fd1a0ac52a7e76f196912f81d1f
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-28 15:21:17 -0700

    Creating Forgot Password related files
    - Creating activity, layout and manifest declaration of activity

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/activity/ForgotPasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.avengers.enterpriseexpensetracker.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avengers.enterpriseexpensetracker.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}

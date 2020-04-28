2b7bb83e88a3226d95a9eed2dc52e754684e1402
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-27 18:05:04 -0700

    Adapter for notifications

---
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/adapter/NotificationAdapter.kt
@@ -0,0 +1,31 @@
+package com.avengers.enterpriseexpensetracker.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.adapter.viewholder.NotificationViewHolder
+import com.avengers.enterpriseexpensetracker.modal.Notification
+
+class NotificationAdapter(private var notifications: List<Notification>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        lateinit var viewHolder: RecyclerView.ViewHolder
+        val inflater = LayoutInflater.from(parent.context)
+        val reqViewHolder = inflater.inflate(R.layout.item_notification, parent, false)
+        viewHolder = NotificationViewHolder(reqViewHolder)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return notifications.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is NotificationViewHolder) {
+            holder.getDateView().text = notifications[position].getDate()
+            holder.getMessageView().text = notifications[position].getNotificationMessage()
+        }
+    }
+}

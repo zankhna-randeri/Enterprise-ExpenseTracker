d932c176f47ef30b389a47f17cb03bf560142f67
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-04-29 00:55:08 -0700

    Broadcast receiver to handle request otp response

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/activity/ForgotPasswordActivity.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/activity/ForgotPasswordActivity.kt
@@ -1,7 +1,11 @@
 package com.avengers.enterpriseexpensetracker.ui.activity
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
@@ -9,7 +13,10 @@ import android.widget.TextView
 import android.widget.ViewSwitcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.avengers.enterpriseexpensetracker.R
+import com.avengers.enterpriseexpensetracker.modal.response.ApiResponse
+import com.avengers.enterpriseexpensetracker.receiver.ApiResponseReceiver
 import com.avengers.enterpriseexpensetracker.service.EETrackerJobService
 import com.avengers.enterpriseexpensetracker.util.Constants
 import com.avengers.enterpriseexpensetracker.util.Utility
@@ -27,11 +34,13 @@ class ForgotPasswordActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var inputConfirmNewPassword: TextInputLayout
     private lateinit var toolbar: Toolbar
     private lateinit var title: TextView
+    private var requestOTPResponseReceiver: BroadcastReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
         initView()
+        initBroadcast()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -40,6 +49,19 @@ class ForgotPasswordActivity : AppCompatActivity(), View.OnClickListener {
             return true
         }
         return false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestOTPResponseReceiver?.let {
+            val intentFilter = IntentFilter(Constants.BROADCAST_REQUEST_OTP)
+            LocalBroadcastManager.getInstance(this).registerReceiver(it, intentFilter)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        requestOTPResponseReceiver?.let { LocalBroadcastManager.getInstance(this).unregisterReceiver(it) }
     }
 
     private fun initView() {
@@ -72,6 +94,7 @@ class ForgotPasswordActivity : AppCompatActivity(), View.OnClickListener {
                 inputEmail.error = null
                 val emailId = inputEmail.editText?.text.toString()
                 if (Utility.getInstance().isValidEmail(emailId)) {
+                    //TODO: Show Loading view
                     val otp = generateOTP(7)
                     val intent = Intent(this, EETrackerJobService::class.java).apply {
                         putExtra(Constants.EXTRA_EMAIL, emailId)
@@ -98,5 +121,30 @@ class ForgotPasswordActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         return otp
+    }
+
+    private fun initBroadcast() {
+        requestOTPResponseReceiver = object : ApiResponseReceiver() {
+            override fun onSuccess(context: Context?, response: ApiResponse) {
+                viewSwitcher.showNext()
+            }
+
+            override fun onFailure(context: Context?, message: String?) {
+                context?.let { Utility.getInstance().showMsg(it, message) }
+            }
+
+            override fun onReceive(context: Context?, intent: Intent?) {
+                // TODO: hideLoadingView
+                val response = intent?.getParcelableExtra<ApiResponse>(Constants.EXTRA_API_RESPONSE)
+                response?.let {
+                    Log.d(Constants.TAG, "response $response")
+                    if (response.isSuccess()) {
+                        onSuccess(context, response)
+                    } else {
+                        onFailure(context, context?.getString(R.string.txt_api_failed))
+                    }
+                }
+            }
+        }
     }
 }

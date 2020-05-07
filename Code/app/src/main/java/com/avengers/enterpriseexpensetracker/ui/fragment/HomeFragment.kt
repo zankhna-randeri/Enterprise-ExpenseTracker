09110b993b48bed48a8634de4e6cea3d69c150e6
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-05-06 17:50:54 -0700

    Removed unnecessary method

--- Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/HomeFragment.kt
+++ Code/app/src/main/java/com/avengers/enterpriseexpensetracker/ui/fragment/HomeFragment.kt
@@ -143,9 +143,6 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
                                             report)
                                 view?.findNavController()?.navigate(action)
                             }
-
-                            override fun btnViewReceiptClickListener(position: Int) {
-                            }
                         })
             }
         pendingExpenseView?.adapter = adapter

5259d77bbf1d53242f964e4fcfee17cc450067bd
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-05-13 16:01:06 -0700

    Unit test to verify correct date parsing

---
+++ Code/app/src/test/java/com/avengers/enterpriseexpensetracker/DateFormatterTest.kt
@@ -0,0 +1,58 @@
+package com.avengers.enterpriseexpensetracker
+
+import com.avengers.enterpriseexpensetracker.util.EETrackerDateFormatManager
+import org.junit.Assert
+import org.junit.Test
+
+class DateFormatterTest {
+
+    private val dateFormatMgr = EETrackerDateFormatManager()
+
+    @Test
+    fun parseMonthDateOrdinalTest() {
+        val answer = dateFormatMgr.parseDate("January 3rd 2020")
+        Assert.assertEquals("01/03/2020", answer)
+    }
+
+    @Test
+    fun parseMonthDateWithoutOrdinalTest() {
+        val answer = dateFormatMgr.parseDate("January 3 2020")
+        Assert.assertEquals("01/03/2020", answer)
+    }
+
+    @Test
+    fun parseDateWithoutOrdinalMonthTest() {
+        val answer = dateFormatMgr.parseDate("12 January 2020")
+        Assert.assertEquals("01/12/2020", answer)
+    }
+
+    @Test
+    fun parseDateWithOrdinalMonthTest() {
+        val answer = dateFormatMgr.parseDate("1st January 2020")
+        Assert.assertEquals("01/01/2020", answer)
+    }
+
+    @Test
+    fun parseDateWithOrdinalSmallMonthTest() {
+        val answer = dateFormatMgr.parseDate("1st Jan 2020")
+        Assert.assertEquals("01/01/2020", answer)
+    }
+
+    @Test
+    fun parseSmallMonthDateWithOrdinalTest() {
+        val answer = dateFormatMgr.parseDate("Feb 22nd 2020")
+        Assert.assertEquals("02/22/2020", answer)
+    }
+
+    @Test
+    fun parseSmallMonthDateWithoutOrdinalTest() {
+        val answer = dateFormatMgr.parseDate("Feb 22 2020")
+        Assert.assertEquals("02/22/2020", answer)
+    }
+
+    @Test
+    fun parseDateWithoutOrdinalSmallMonthTest() {
+        val answer = dateFormatMgr.parseDate("13 Jan 2020")
+        Assert.assertEquals("01/13/2020", answer)
+    }
+}

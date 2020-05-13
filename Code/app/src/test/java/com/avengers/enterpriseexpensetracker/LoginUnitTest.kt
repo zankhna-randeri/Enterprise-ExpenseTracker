ed16b7533831f39e94f7c39668016fb5cf4ff8f6
Zankhna Randeri <zankhna.randeri@gmail.com>
2020-05-13 15:58:21 -0700

    Bug fix with new class

--- Code/app/src/test/java/com/avengers/enterpriseexpensetracker/LoginUnitTest.kt
+++ Code/app/src/test/java/com/avengers/enterpriseexpensetracker/LoginUnitTest.kt
@@ -2,7 +2,7 @@ package com.avengers.enterpriseexpensetracker
 
 import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.avengers.enterpriseexpensetracker.ui.activity.LoginActivity
+import com.avengers.enterpriseexpensetracker.util.Utility
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -14,40 +14,33 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.P], manifest = Config.NONE)
 class LoginUnitTest {
 
-    private val loginActivity =
-        LoginActivity()
-
     @Test
     fun emptyEmailWithWhiteSpaceTest() {
-        val email = "   "
-        assert(!loginActivity.isValidEmail(email))
+        assert(!Utility.getInstance().isValidEmail("   "))
     }
 
     @Test
     fun emptyEmailTest() {
-        val email = ""
-        assert(!loginActivity.isValidEmail(email))
+        assert(!Utility.getInstance().isValidEmail(""))
     }
 
     @Test
     fun invalidEmailTest1() {
-        val email = "abc@abc.com@"
-        assert(!loginActivity.isValidEmail(email))
+        assert(!Utility.getInstance().isValidEmail("abc@abc.com@"))
     }
 
     @Test
     fun invalidEmailTest2() {
-        assert(!loginActivity.isValidEmail("abcabc.abc.com"))
+        assert(!Utility.getInstance().isValidEmail("abcabc.abc.com"))
     }
 
     @Test
     fun invalidEmailTest3() {
-        assert(!loginActivity.isValidEmail("abc@@abc.abc.com"))
+        assert(!Utility.getInstance().isValidEmail("abc@@abc.abc.com"))
     }
 
     @Test
     fun validEmailTest() {
-        val email = "abc@abc.com"
-        assert(loginActivity.isValidEmail(email))
+        assert(Utility.getInstance().isValidEmail("abc@abc.com"))
     }
 }
